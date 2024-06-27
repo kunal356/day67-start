be4d1c5410bed6819eb7ffee8619c1d168e931a3
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-06-27 11:04:34 +0100

    516. Requirement 4- Be Able DELETE Blog Posts

--- main.py
+++ main.py
@@ -124,8 +124,14 @@ def edit_post(post_id):
 
     return render_template("make-post.html", form=form, mode="edit")
 
-    # TODO: delete_post() to remove a blog post from the database
 
+# TODO: delete_post() to remove a blog post from the database
+@app.route('/delete-post/<post_id>', methods=['POST', 'GET', 'DELETE'])
+def delete_post(post_id):
+    post_to_delete = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
     # Below is the code from previous lessons. No changes needed.
 
 
